96cd80a8f61c928802827ba556a5f7ab9371b7f6
qsaucy <quentin.saucy@heig-vd.ch>
2020-09-23 13:35:14 +0200

    first session test

--- src/main/java/ch/heigvd/amt/project1/presentation/LoginServlet.java
+++ src/main/java/ch/heigvd/amt/project1/presentation/LoginServlet.java
@@ -5,6 +5,7 @@ import ch.heigvd.amt.project1.business.Checker;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 @WebServlet("/login.do")
 public class LoginServlet extends javax.servlet.http.HttpServlet {
@@ -16,15 +17,33 @@ public class LoginServlet extends javax.servlet.http.HttpServlet {
         business = new Checker();
     }
     protected  void doGet (javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+            HttpSession session = request.getSession(true);
+            if(session.getAttribute("login")==null)
+                request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
 
-        request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
+        else{
+
+            request.getRequestDispatcher("/index.jsp").forward(request, response);
+        }
+
     }
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        String login = request.getParameter("uname");
-        String password = request.getParameter("psw");
-        String model = business.login(login,password);
-        request.setAttribute("login", model);
-        request.setAttribute("uname",request.getParameter("uname"));
-        request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
+        HttpSession session = request.getSession(true);
+
+        if (session.getAttribute("login")==null) {
+
+            session=request.getSession();
+            String login = request.getParameter("uname");
+            String password = request.getParameter("psw");
+            String model = business.login(login, password);
+            session.setAttribute("login",login);
+            request.setAttribute("login", model);
+            request.setAttribute("uname", request.getParameter("uname"));
+            request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
+        }
+        else {
+            request.setAttribute("login", session.getAttribute("login"));
+            request.getRequestDispatcher("/WEB-INF/pages/login.jsp").forward(request, response);
+        }
     }
 }
